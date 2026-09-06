5a28b7f7eda1c43ebe5551f81ce2bee2fe5dfee3
Barch27 <eliyaswesenu1994@gmail.com>
2026-09-06 21:53:17 +0300

    refactor(app): remove unused routes and server start logic

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 import express from "express";
-import userRoutes from "./routes/user.routes.js";
-import authRoutes from "./routes/auth.routes.js";
+
 import { requestId } from "./middleware/requestId.middleware.js";
 import { errorHandler } from "./middleware/error.middleware.js";
 import { requestLogger } from "./middleware/logger.middleware.js";
-import { env } from "./config/env.js"
+
 
 const app = express();
 
@@ -17,17 +16,6 @@ app.use(requestLogger);
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 
-
-app.get("/", (req, res) => {
-  res.json({
-    message: "ProjectHub API Running",
-  });
-});
-
 app.use(errorHandler);
 
-const PORT = env.PORT;
-
-app.listen(PORT, () => {
-  console.log("Server running on port 3000");
-});
+export default app;
